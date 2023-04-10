b50c44f3ed71bf0c5217a4efd4a2bbae184dcf69
Franco Tiveron <franco@horthfinancial.com>
2023-04-10 16:18:42 +1000

    --

--- src/WeakEvent.FSharp/WeakEvent.fs
+++ src/WeakEvent.FSharp/WeakEvent.fs
@@ -1,30 +1,37 @@
-﻿namespace Microsoft.FSharp.Control
+﻿namespace WeakEvent.FSharp
 
 open System
 open WeakEvent
 
-type IWeakEvent<'Args> =
-    inherit IEvent<EventHandler<'Args>, 'Args>
-    abstract AddHandler: lifetimeObj: obj * handler: EventHandler<'Args> -> unit
+type IDelegateWeakEvent<'Delegate when 'Delegate :> System.Delegate> =
+    abstract AddHandler: handler: 'Delegate -> unit
+    abstract AddHandler: lifetimeObj: obj * handler: 'Delegate -> unit
+    abstract RemoveHandler: handler: 'Delegate -> unit
 
-[<CompiledName("FSharpWeekEvent`1")>]
+type IWeakEvent<'Delegate,'Args when 'Delegate: delegate<'Args,unit> and 'Delegate :> Delegate> =
+    inherit IObservable<'Args>
+    inherit IDelegateWeakEvent<'Delegate>
+
+type IWeakEvent<'Args> =
+    inherit IWeakEvent<EventHandler<'Args>, 'Args>
+
+[<CompiledName("FSharpWeakEvent`1")>]
 type WeakEvent<'T>() =
     let weakEventSource = WeakEventSource<'T>()
-
+     
     member this.Trigger(arg: 'T) = weakEventSource.Raise(this, arg)
-    member this.Raise = this.Trigger
+    member this.Trigger(arg: 'T, exceptionHandler) = weakEventSource.Raise(this, arg, exceptionHandler)
 
-    member __.Publish = {
-        new IWeakEvent<'T> with
-            member __.AddHandler(d) = weakEventSource.Subscribe(d)
-            member __.AddHandler(l, d) = weakEventSource.Subscribe(l, d)
-            member __.RemoveHandler(d) = weakEventSource.Unsubscribe(d)
+    member __.Publish =
+        { new IWeakEvent<'T> with
+              member __.AddHandler(d) = weakEventSource.Subscribe(d)
+              member __.AddHandler(l, d) = weakEventSource.Subscribe(l, d)
+              member __.RemoveHandler(d) = weakEventSource.Unsubscribe(d)
 
-            member this.Subscribe(observer) =
-                let h = EventHandler<_>(fun _ args -> observer.OnNext(args))
-                (this :?> IEvent<_, _>).AddHandler(h)
-                { new System.IDisposable with member __.Dispose() = (this :?> IEvent<_, _>).RemoveHandler(h) } }
+              member this.Subscribe(observer) =
+                  let h = new EventHandler<_>(fun _ args -> observer.OnNext(args))
+                  (this :?> IWeakEvent<_>).AddHandler(h)
 
-module Event = 
-    let addWeak callback lifetimeObj (sourceEvent: IWeakEvent<'Args>) =
-        sourceEvent.AddHandler(lifetimeObj, EventHandler<_>(callback))
+                  { new IDisposable with
+                      member x.Dispose() =
+                          (this :?> IWeakEvent<_>).RemoveHandler(h) } }
---
+++ src/WeakEvent.FSharp/WeakEventModule.fs
@@ -0,0 +1,99 @@
+﻿namespace WeakEvent.FSharp
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
+module WeakEvent =
+    [<CompiledName("Create")>]
+    let create<'T> () =
+        let ev = new WeakEvent<'T>()
+        ev.Trigger, ev.Publish
+
+    [<CompiledName("Map")>]
+    let map mapping (sourceEvent: IWeakEvent<'Delegate, 'T>) =
+        let ev = new WeakEvent<_>()
+        sourceEvent.Add(fun x -> ev.Trigger(mapping x))
+        ev.Publish
+
+    [<CompiledName("Filter")>]
+    let filter predicate (sourceEvent: IWeakEvent<'Delegate, 'T>) =
+        let ev = new WeakEvent<_>()
+
+        sourceEvent.Add(fun x ->
+            if predicate x then
+                ev.Trigger x)
+
+        ev.Publish
+
+    [<CompiledName("Partition")>]
+    let partition predicate (sourceEvent: IWeakEvent<'Delegate, 'T>) =
+        let ev1 = new WeakEvent<_>()
+        let ev2 = new WeakEvent<_>()
+
+        sourceEvent.Add(fun x ->
+            if predicate x then
+                ev1.Trigger x
+            else
+                ev2.Trigger x)
+
+        ev1.Publish, ev2.Publish
+
+    [<CompiledName("Choose")>]
+    let choose chooser (sourceEvent: IWeakEvent<'Delegate, 'T>) =
+        let ev = new WeakEvent<_>()
+
+        sourceEvent.Add(fun x ->
+            match chooser x with
+            | None -> ()
+            | Some r -> ev.Trigger r)
+
+        ev.Publish
+
+    [<CompiledName("Scan")>]
+    let scan collector state (sourceEvent: IWeakEvent<'Delegate, 'T>) =
+        let mutable state = state
+        let ev = new WeakEvent<_>()
+
+        sourceEvent.Add(fun msg ->
+            let z = state
+            let z = collector z msg
+            state <- z
+            ev.Trigger(z))
+
+        ev.Publish
+
+    [<CompiledName("Add")>]
+    let add callback (sourceEvent: IWeakEvent<'Delegate, 'T>) =
+        sourceEvent.Add(callback)
+
+    [<CompiledName("Pairwise")>]
+    let pairwise (sourceEvent: IWeakEvent<'Delegate, 'T>) : IWeakEvent<'T * 'T> =
+        let ev = new WeakEvent<'T * 'T>()
+        let mutable lastArgs = None
+
+        sourceEvent.Add(fun args2 ->
+            (match lastArgs with
+             | None -> ()
+             | Some args1 -> ev.Trigger((args1, args2)))
+
+            lastArgs <- Some args2)
+
+        ev.Publish
+
+    [<CompiledName("Merge")>]
+    let merge (event1: IWeakEvent<'Del1, 'T>) (event2: IWeakEvent<'Del2, 'T>) =
+        let ev = new WeakEvent<_>()
+        event1.Add(fun x -> ev.Trigger(x))
+        event2.Add(fun x -> ev.Trigger(x))
+        ev.Publish
+
+    [<CompiledName("Split")>]
+    let split (splitter: 'T -> Choice<'U1, 'U2>) (sourceEvent: IWeakEvent<'Delegate, 'T>) =
+        let ev1 = new WeakEvent<_>()
+        let ev2 = new WeakEvent<_>()
+
+        sourceEvent.Add(fun x ->
+            match splitter x with
+            | Choice1Of2 y -> ev1.Trigger(y)
+            | Choice2Of2 z -> ev2.Trigger(z))
+
+        ev1.Publish, ev2.Publish
